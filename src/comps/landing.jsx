5f9ded40e8bac91bcd264e3ad3e81e25d6bc7d5f
philakagujones <philakagujones@gmail.com>
2022-08-02 23:33:29 -0700

    finalized center text

--- src/comps/landing.jsx
+++ src/comps/landing.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-// import Background from '../images/landing.webp'
 import '../styles/landing.css'
 
 function Landing() {
   return (
     <div className='background'>
-      {/* <img src={Background} alt='Landing Image' className='background' /> */}
       <div className="centertext">
         <span>
           <h1>LATTER HOUSE</h1>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Footer from '../comps/footer'
 import Landing from '../comps/landing'
+import Navbar from '../comps/navbar'
 import '../styles/home.css'
 
 function Home() {
   return (
     <div>
-        <Landing />
-        <Footer />
+      <Navbar />
+      <Landing />
+      <Footer />
     </div>
   )
 }
