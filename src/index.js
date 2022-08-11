75c268e010e605ea1f7857c3759aba7a392a6bf5
philakagujones <philakagujones@gmail.com>
2022-08-10 22:54:28 -0700

    fixed page scroll issue

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import App from './App';
+import ScrollToTop from './scroll';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ScrollToTop />
+        <App />
     </BrowserRouter>
   </React.StrictMode>
 );
