7cdff5fccd0105521bcad86f67bb66044ef9dc68
philakagujones <philakagujones@gmail.com>
2022-08-19 18:34:04 -0700

    about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import Footer from '../comps/footer'
-import Latter from '../comps/latter'
 import Navbar from '../comps/navbar'
-import '../styles/about.css'
+import styles from '../styles/about.module.css'
 
 function About() {
   return (
     <>
-      <div className="container">
-        <Navbar />
-        <Latter />
+      <Navbar /> 
+      <div className={styles.container}>
+        <div className={styles.text}>
+          <p>
+            LATTER HOUSE IS A LIVING BREATHING ENTITY EMERGING FROM VANCOUVER, BC. OUR TEAM IS DEDICATED TO PROVIDING THE 
+            BRIDGE BETWEEN STREETWEAR AND LUXURY. WE INVITE YOU ON THIS JOURNEY WITH US AND HOPEFULLY YOU ENJOY THE PIECES WE CREATE AS WELL
+            AS THE STORIES. 
+            <br />
+            <br />
+            THE GLORY OF THE LATTER HOUSE SHALL BE GREATER THAN OF THE FORMER
+          </p>
+        </div>
       </div>
 
       <Footer />
